f7d8e230438a8fe4f4713f1e1ad6b9f76959d4bc
Jonas Jensen <jonas.jensen@gmail.com>
2013-06-18 09:22:49 +0200

    * change copyright text to standard for the remaining drivers

--- drivers/dma/moxart-dma.c
+++ drivers/dma/moxart-dma.c
@@ -1,8 +1,14 @@
-/* Copyright (C) 2013 Jonas Jensen <jonas.jensen@gmail.com>
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version. */
+/*
+ * MOXA ART SoCs DMA driver.
+ *
+ * Copyright (C) 2013 Jonas Jensen
+ *
+ * Jonas Jensen <jonas.jensen@gmail.com>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
 
 #include <linux/dmaengine.h>
 #include <linux/dma-mapping.h>
